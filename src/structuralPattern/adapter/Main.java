f9e3850620efc81373aa6c399051720f741be1ea
SLCube <kgm0096@naver.com>
2023-06-02 14:35:23 +0900

    adapter pattern에 대한 설명 추가

--- src/structuralPattern/adapter/Main.java
+++ src/structuralPattern/adapter/Main.java
@@ -1,5 +1,10 @@
 package structuralPattern.adapter;
 
+/**
+ * adapter pattern
+ *
+ * 어댑터 패턴이란 서로 호환성이 없는 인터페이스를 사용하는 클래스에서 호환되게끔 해주는 패턴이다.
+ */
 public class Main {
     public static void main(String[] args) {
         IphoneUser userA = new IphoneUser(new EarPhoneAdapter(new ThreePointFiveEarPhone()));
