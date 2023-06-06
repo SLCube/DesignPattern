94b51c41a6448e6379841ff7b6afa53421aeeb18
SLCube <kgm0096@naver.com>
2023-06-06 16:41:18 +0900

    Template Method Pattern 학습

---
+++ src/behavioral_pattern/template_method/ConcreteClassA.java
@@ -0,0 +1,8 @@
+package behavioral_pattern.template_method;
+
+public class ConcreteClassA extends AbstractClass {
+    @Override
+    public void primitiveOperation() {
+        System.out.println("Primitive Operation A");
+    }
+}
