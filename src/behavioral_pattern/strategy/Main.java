b005756b36bffe80d8fbbf890455caf7fc0889c3
SLCube <kgm0096@naver.com>
2023-06-06 16:28:20 +0900

    문서 수정

--- src/behavioral_pattern/strategy/Main.java
+++ src/behavioral_pattern/strategy/Main.java
@@ -7,7 +7,7 @@ package behavioral_pattern.strategy;
  * <p>
  * 공통로직이 부모클래스에 있지 않고 context라는 별개의 클래스에 있기 때문에 전략구현체에 대한 영향도가 적음.
  * context가 strategy라는 인터페이스에 의존하고 있기 때문에 갈아 끼우기 쉬움.
- *
+ * <p>
  * 로직이 늘어날 때 마다 구현체 클래스가 늘어남.
  * context와 strategy를 한번 조립하면 전략을 변경하기 힘듦.
  */
