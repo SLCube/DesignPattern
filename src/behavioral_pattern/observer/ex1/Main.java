54621b73a09c13c576f0d2658ac71e303351b14d
SLCube <kgm0096@naver.com>
2023-06-06 14:08:57 +0900

    Observer pattern 정리

--- src/behavioral_pattern/observer/ex1/Main.java
+++ src/behavioral_pattern/observer/ex1/Main.java
@@ -1,5 +1,13 @@
 package behavioral_pattern.observer.ex1;
 
+/**
+ * Observer Pattern
+ *
+ * N : 1의 관계이면서 N이 1의 변화를 알아야하고 변화로 인해 또 다른 행동을 취해야할때 사용하면 좋다.
+ * observer와 subject사이엔 느슨한 결합만이 존재하기 때문에 시스템이 유연해진다.
+ *
+ * 남용시 상태관리가 힘들어지며, 데이터 배분을 잘못하게되면 예상치 못한 문제가 발생할 수 있다.
+ */
 public class Main {
     public static void main(String[] args) {
         Order order = new Order();
